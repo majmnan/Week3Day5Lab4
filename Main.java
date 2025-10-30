e8bacd9498e387c14d7d466613b60d70d657e18a
Mohannad Mannan <majmnan@gmail.com>
2025-10-30 11:42:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static Scanner in = new Scanner(System.in);
+public class Main { 
     public static void main(String[] args) {
         ArrayList<Circle> circles= new ArrayList<>();
         circles.add(new Circle(10));
@@ -24,28 +21,36 @@ public class Main {
         triangles.add(new Triangle(70,15));
         triangles.get(2).setHeight(triangles.get(2).getBase());
 
-        displayShapes(circles,rectangles,triangles);
+        displayCircles(circles);
+        displayRectangles(rectangles);
+        displayTriangles(triangles);
+
 
     }
 
-    public static void displayShapes(ArrayList<Circle> circles, ArrayList<Rectangle> rectangles, ArrayList<Triangle> triangles){
-        System.out.println("====================Circles part====================");
-        for (int i = 0; i<circles.size(); i++){
-            Circle c = circles.get(i);
-            System.out.print(i+1 + "-");
-            System.out.println(c);
+        public static void displayCircles(ArrayList<Circle> circles){
+            System.out.println("====================Circles====================");
+            for (int i = 0; i<circles.size(); i++){
+                Circle c = circles.get(i);
+                System.out.print(i+1 + "-");
+                System.out.println(c);
+            }
+
         }
-        System.out.println("====================Rectangles part====================");
-        for (int i = 0; i<rectangles.size(); i++){
-            Rectangle r = rectangles.get(i);
-            System.out.print(i+1 + "-");
-            System.out.println(r);
+        public static void displayRectangles(ArrayList<Rectangle> rectangles){
+            System.out.println("====================Rectangles====================");
+            for (int i = 0; i<rectangles.size(); i++){
+                Rectangle r = rectangles.get(i);
+                System.out.print(i+1 + "-");
+                System.out.println(r);
+            }
         }
-        System.out.println("====================Triangles part====================");
-        for(int i = 0; i<triangles.size(); i++){
-            Triangle t = triangles.get(i);
-            System.out.print(i+1 + "-");
-            System.out.println(t);
+        public static void displayTriangles(ArrayList<Triangle> triangles){
+            System.out.println("====================Triangles====================");
+            for(int i = 0; i<triangles.size(); i++){
+                Triangle t = triangles.get(i);
+                System.out.print(i+1 + "-");
+                System.out.println(t);
+            }
         }
-    }
 }
